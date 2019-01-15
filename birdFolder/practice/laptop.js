f69772697b02d1b4f6bc595234f9d5e9bb2d224e
melissatanja <43143959+melissatanja@users.noreply.github.com>
2019-01-15 12:16:09 -0500

    excorcised the cursed folder

---
+++ birdFolder/practice/laptop.js
@@ -0,0 +1,52 @@
+// server variables
+
+var dataServer;
+var subKey = 'sub-c-920db3fa-1363-11e9-a843-26c2824ead74';
+
+//name used to sort your messages. used like a radio station. can be called anything
+var channelName = "movement";
+
+function setup() 
+{
+  // getAudioContext().resume();
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  
+
+
+   // initialize pubnub
+  dataServer = new PubNub(
+  {
+    subscribe_key : subKey,  
+    ssl: true  //enables a secure connection. This option has to be used if using the OCAD webspace
+  });
+  
+  //attach callbacks to the pubnub object to handle messages and connections
+  dataServer.addListener({ message: readIncoming });
+  dataServer.subscribe({channels: [channelName]});
+
+
+    //display a waiting message
+    background(255);
+    noStroke();
+    fill(0);  
+    textSize(30)
+    text("Waiting", width/2, height/2);
+
+}
+
+function draw() 
+{
+
+text("no angle info");
+
+}
+
+function readIncoming(inMessage) //when new data comes in it triggers this function, 
+{                               
+  background(255);
+  text('x pos: ' + inMessage.message.xVal, width/2, height/4);
+  text('y pos: ' + inMessage.message.yVal, width/2, height/2);
+
+}
+
---
+++ birdFolder/practice/phone.js
@@ -0,0 +1,66 @@
+// server variables
+
+var dataServer;
+var pubKey = 'pub-c-182a2099-8001-444d-9c8f-35f7b37c8846';
+var subKey = 'sub-c-920db3fa-1363-11e9-a843-26c2824ead74';
+
+//name used to sort your messages. used like a radio station. can be called anything
+var channelName = "movement";
+
+function setup() 
+{
+  getAudioContext().resume();
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  
+  
+
+   // initialize pubnub
+  dataServer = new PubNub(
+  {
+    publish_key   : pubKey,  //get these from the pubnub account online
+    subscribe_key : subKey,  
+    ssl: true  //enables a secure connection. This option has to be used if using the OCAD webspace
+  });
+
+  setInterval(getOrientation, 300);
+
+}
+
+function draw() 
+{
+
+  x+=accelerationX*0.05;
+  y+=accelerationY*0.05;
+  z+=accelerationZ*0.05;
+  normalMaterial();
+  rotateX(x);
+  rotateY(y);
+  rotateZ(z);
+  box(200, 200, 200);
+
+}
+
+
+//sends from the button press
+function getOrientation() {
+
+  // var xVal = rotationY;
+  // var yVal = rotationX;
+
+//console.log(slideNumber);
+
+  //publish the number to everyone.
+  dataServer.publish(
+    {
+      channel: channelName,
+      message: 
+      {
+        xVal: rotationY,
+        yVal: rotationX      
+      }
+    });
+
+}
+
+
