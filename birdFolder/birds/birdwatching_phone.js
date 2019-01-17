9edd7a3c50b7d3ae87807adbbf14b4583c4a7396
melissatanja <43143959+melissatanja@users.noreply.github.com>
2019-01-17 16:21:54 -0500

    update

--- birdFolder/birds/birdwatching_phone.js
+++ birdFolder/birds/birdwatching_phone.js
@@ -106,7 +106,7 @@ function sendData() {
     {
       user: "red",
       x_angle: xPos, 
-      y_angle: yPos
+      y_angle: yPos,
       // red_bird: , 
       // blue_bird: , 
       // green_bird: , 
