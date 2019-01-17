77e70151e2bb27c839ac9d2020fca5ed479dcfcd
melissatanja <43143959+melissatanja@users.noreply.github.com>
2019-01-17 11:03:17 -0500

    realdata

--- birdFolder/birds/birdwatching_laptop.js
+++ birdFolder/birds/birdwatching_laptop.js
@@ -183,7 +183,7 @@ function readIncoming(inMessage) //when new data comes in it triggers this funct
 
     if(inMessage.message.user = "red"){
       
-      var moveX = map(inMessage.message.x_angle, -10, 10, 0, width/2);
+      var moveX = map(inMessage.message.x_angle, -10, 10,  0, width/2);
       var moveY = map(inMessage.message.y_angle, -10, 10, 0, height/2);
 
       image(img_red, inMessage.message.x_angle, inMessage.message.y_angle);
