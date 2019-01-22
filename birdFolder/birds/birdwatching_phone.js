e007a7f19a64a9c3b157c695c0e2475cb19cf243
melissatanja <43143959+melissatanja@users.noreply.github.com>
2019-01-22 05:38:34 -0500

    update

--- birdFolder/birds/birdwatching_phone.js
+++ birdFolder/birds/birdwatching_phone.js
@@ -85,69 +85,69 @@ function draw() {
 // blue_bird = 2;
 // green_bird = 1;
 
-var redCount = 1;
-var blueCount = 1;
-var greenCount = 1;
-var yellowCount = 1;
+// var redCount = 1;
+// var blueCount = 1;
+// var greenCount = 1;
+// var yellowCount = 1;
 
 if(redCount != undefined){
 
   // text("red: " + redCount, width/5, height/2);
 
-  for(var r=0;r<=3;r++){
+  // for(var r=0;r<=3;r++){
     fill("red");
     // ellipse(map(width/4*r, 0, w*3/4,300, w-300),height/5-100,100,100);
     if(redCount > 0){
       ellipse((width/5) * redCount, height/8, 50, 50);
     }
-    if(r+1 == redCount){
-      break;}
-  }
+  //   if(r+1 == redCount){
+  //     break;}
+  // }
 }
 
 if(blueCount != undefined){
 
   // text("blue: " + blueCount, (width/5) * 2, height/2);
 
-  for(var b=0;b<=3;b++){
+  // for(var b=0;b<=3;b++){
     fill("blue");
     // ellipse(map(width/4*b,0,w*3/4,300,w-300),2*height/5-100,100,100);
     if(blueCount > 0){
       ellipse((width/5) * blueCount, height/8 * 2, 50, 50);
     }
-    if(b+1 == blueCount){
-      break;}
-  }
+  //   if(b+1 == blueCount){
+  //     break;}
+  // }
 }
 
 if(greenCount != undefined){
 
   // text("green: " + greenCount, (width/5) * 3, height/2);
 
-  for(var g=0;g<=3;g++){
+  // for(var g=0;g<=3;g++){
     fill("green");
     // ellipse(map(width/4*g,0,w*3/4,300,w-300),3*height/5-100,100,100);
     if(greenCount > 0){
       ellipse((width/5) * greenCount, height/8 * 3, 50, 50);
     }
-    if(g+1 == greenCount){
-      break;}
-  } 
+  //   if(g+1 == greenCount){
+  //     break;}
+  // } 
 }
 
 if(yellowCount != undefined){
 
   // text("yellow: " + yellowCount, (width/5) * 4, height/2);         
 
-  for(var y=0;y<=3;y++){
+  // for(var y=0;y<=3;y++){
     fill("yellow");
     // ellipse(map(width/4*y,0,w*3/4,300,w-300),4*height/5-100,100,100);
     if(yellowCount > 0){
       ellipse((width/5) * yellowCount, height/8 * 4, 50, 50);
     }
-    if(y+1 == yellowCount){
-      break;}
-  }  
+    // if(y+1 == yellowCount){
+      // break;}
+  // }  
 }
 
 }
