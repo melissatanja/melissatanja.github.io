d393d51139d94460c3befbccbc125d8ce50cf4a1
melissatanja <43143959+melissatanja@users.noreply.github.com>
2019-01-22 09:06:51 -0500

    update

---
+++ birdFolder/birds/trade.js
@@ -0,0 +1,78 @@
+function trade(){
+
+
+
+}
+
+// function tradeRequest(){
+
+//   //red user 
+//   if(user === "red"){
+
+//     //blue trade button pressed
+//     if(trade_blue === 1){
+
+//       if(blue_bird > 0){
+
+//         //giving blue bird
+//         blue_bird -= 1;
+
+//         //blue user
+//         if(user = 1){
+
+//           //receiving blue bird
+//           blue_bird += 1;
+
+//         }
+//       }
+
+//     }else if(green_bird > 0){
+
+//       //trading green bird
+//       green_bird -= 1;
+
+//       if(user = 1){
+
+//         //receiving green bird
+//         green_bird += 1;
+
+//       }
+
+//     }else if(yellow_bird > 0){
+
+//       //trading yellow bird
+//       yellow_bird -= 1;
+
+//       if(user = 1){
+
+//         //receiving yellow bird
+//         yellow_bird += 1;
+
+//       }
+
+//     }else{
+
+//       window.alert("No birds available");
+
+//     }
+
+//   }
+// }
+
+//     //trading with green user
+//     if(trade_green === true){
+
+
+
+//     }
+
+//     //trading with yellow user
+//     if(trade_yellow === true){
+
+
+
+//     }
+
+//   }
+
+// }
