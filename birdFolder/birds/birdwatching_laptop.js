489c1bf3c1230169daaa8b96c9dbd00bacf7170b
melissatanja <43143959+melissatanja@users.noreply.github.com>
2019-01-22 02:29:53 -0500

    update

--- birdFolder/birds/birdwatching_laptop.js
+++ birdFolder/birds/birdwatching_laptop.js
@@ -179,7 +179,7 @@ if(xposition != undefined && yposition != undefined){
   image(bg, cs/2, cs/2, cs - 50, cs - 50);
 
   //red binoculars
-  // image(img_red, X, Y, r, r);
+  image(img_red, X, Y, r, r);
 
   for(let i=0;i<birdsBlue.length;i++){
     fill("blue");
