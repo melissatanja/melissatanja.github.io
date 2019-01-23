a649e7279355c377a2ac19ef2e5448440f78c52b
melissatanja <43143959+melissatanja@users.noreply.github.com>
2019-01-23 14:35:06 -0500

    update

--- birdFolder/birds/birdwatching_laptop.js
+++ birdFolder/birds/birdwatching_laptop.js
@@ -205,7 +205,7 @@ function draw()
 
 function birdCatchB(){
   for(let i=0;i<birdsBlue.length;i++){
-    if(dist(birdsBlue[i].x,birdsBlue[i].y, rX, rY) <= bsize+15){
+    if(dist(birdsBlue[i].x,birdsBlue[i].y, rX, rY) <= bsize+5){
       birdsBlue.splice(i,1);
 
       Red[1] += 1;
@@ -215,7 +215,7 @@ function birdCatchB(){
 
 function birdCatchR(){
   for(let i=0;i<birdsRed.length;i++){
-    if(dist(birdsRed[i].x,birdsRed[i].y, rX, rY) <= bsize+15){
+    if(dist(birdsRed[i].x,birdsRed[i].y, rX, rY) <= bsize+5){
       birdsRed.splice(i,1);
 
       Red[0] += 1;
@@ -224,7 +224,7 @@ function birdCatchR(){
 }
 function birdCatchY(){
   for(let i=0;i<birdsYellow.length;i++){
-    if(dist(birdsYellow[i].x,birdsYellow[i].y, rX, rY) <= bsize+15){
+    if(dist(birdsYellow[i].x,birdsYellow[i].y, rX, rY) <= bsize+5){
       birdsYellow.splice(i,1);
 
       Red[3] += 1;
@@ -234,7 +234,7 @@ function birdCatchY(){
 }
 function birdCatchG(){
   for(let i=0;i<birdsGreen.length;i++){
-    if(dist(birdsGreen[i].x,birdsGreen[i].y, rX, rY) <= bsize+15){
+    if(dist(birdsGreen[i].x,birdsGreen[i].y, rX, rY) <= bsize+55){
       birdsGreen.splice(i,1);
 
       Red[2] += 1;
