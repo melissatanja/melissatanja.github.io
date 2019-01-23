0f33792dcd2c7cdd992a3d1c6d7876332c7c8ce6
melissatanja <43143959+melissatanja@users.noreply.github.com>
2019-01-23 18:09:03 -0500

    update

--- birdFolder/birds/bird_catching.js
+++ birdFolder/birds/bird_catching.js
@@ -1,7 +1,7 @@
 //red user
 function birdCatchB(){
   for(let i=0;i<birdsBlue.length;i++){
-    if(dist(birdsBlue[i].x,birdsBlue[i].y, rX, rY) <= bsize+10){
+    if(dist(birdsBlue[i].x,birdsBlue[i].y, rX, rY) <= bsize+15){
       birdsBlue.splice(i,1);
 
       Red[1] += 1;
@@ -11,7 +11,7 @@ function birdCatchB(){
 
 function birdCatchR(){
   for(let i=0;i<birdsRed.length;i++){
-    if(dist(birdsRed[i].x,birdsRed[i].y, rX, rY) <= bsize+10){
+    if(dist(birdsRed[i].x,birdsRed[i].y, rX, rY) <= bsize+15){
       birdsRed.splice(i,1);
 
       Red[0] += 1;
@@ -20,7 +20,7 @@ function birdCatchR(){
 }
 function birdCatchY(){
   for(let i=0;i<birdsYellow.length;i++){
-    if(dist(birdsYellow[i].x,birdsYellow[i].y, rX, rY) <= bsize+10){
+    if(dist(birdsYellow[i].x,birdsYellow[i].y, rX, rY) <= bsize+15){
       birdsYellow.splice(i,1);
 
       Red[3] += 1;
@@ -30,7 +30,7 @@ function birdCatchY(){
 }
 function birdCatchG(){
   for(let i=0;i<birdsGreen.length;i++){
-    if(dist(birdsGreen[i].x,birdsGreen[i].y, rX, rY) <= bsize+10){
+    if(dist(birdsGreen[i].x,birdsGreen[i].y, rX, rY) <= bsize+15){
       birdsGreen.splice(i,1);
 
       Red[2] += 1;
@@ -41,7 +41,7 @@ function birdCatchG(){
 //blue user
 function birdCatchB(){
   for(let i=0;i<birdsBlue.length;i++){
-    if(dist(birdsBlue[i].x,birdsBlue[i].y, bX, bY) <= bsize+10){
+    if(dist(birdsBlue[i].x,birdsBlue[i].y, bX, bY) <= bsize+15){
       birdsBlue.splice(i,1);
 
       Blue[1] += 1;
@@ -51,7 +51,7 @@ function birdCatchB(){
 
 function birdCatchR(){
   for(let i=0;i<birdsRed.length;i++){
-    if(dist(birdsRed[i].x,birdsRed[i].y, bX, bY) <= bsize+10){
+    if(dist(birdsRed[i].x,birdsRed[i].y, bX, bY) <= bsize+15){
       birdsRed.splice(i,1);
 
       Blue[0] += 1;
@@ -60,7 +60,7 @@ function birdCatchR(){
 }
 function birdCatchY(){
   for(let i=0;i<birdsYellow.length;i++){
-    if(dist(birdsYellow[i].x,birdsYellow[i].y, bX, bY) <= bsize+10){
+    if(dist(birdsYellow[i].x,birdsYellow[i].y, bX, bY) <= bsize+15){
       birdsYellow.splice(i,1);
 
       Blue[3] += 1;
@@ -70,7 +70,7 @@ function birdCatchY(){
 }
 function birdCatchG(){
   for(let i=0;i<birdsGreen.length;i++){
-    if(dist(birdsGreen[i].x,birdsGreen[i].y, bX, bY) <= bsize+10){
+    if(dist(birdsGreen[i].x,birdsGreen[i].y, bX, bY) <= bsize+15){
       birdsGreen.splice(i,1);
 
       Blue[2] += 1;
@@ -81,7 +81,7 @@ function birdCatchG(){
 //green user
 function birdCatchB(){
   for(let i=0;i<birdsBlue.length;i++){
-    if(dist(birdsBlue[i].x,birdsBlue[i].y, gX, gY) <= bsize+10){
+    if(dist(birdsBlue[i].x,birdsBlue[i].y, gX, gY) <= bsize+15){
       birdsBlue.splice(i,1);
 
       Green[1] += 1;
@@ -91,7 +91,7 @@ function birdCatchB(){
 
 function birdCatchR(){
   for(let i=0;i<birdsRed.length;i++){
-    if(dist(birdsRed[i].x,birdsRed[i].y, gX, gY) <= bsize+10){
+    if(dist(birdsRed[i].x,birdsRed[i].y, gX, gY) <= bsize+15){
       birdsRed.splice(i,1);
 
       Green[0] += 1;
@@ -100,7 +100,7 @@ function birdCatchR(){
 }
 function birdCatchY(){
   for(let i=0;i<birdsYellow.length;i++){
-    if(dist(birdsYellow[i].x,birdsYellow[i].y, gX, gY) <= bsize+10){
+    if(dist(birdsYellow[i].x,birdsYellow[i].y, gX, gY) <= bsize+15){
       birdsYellow.splice(i,1);
 
       Green[3] += 1;
@@ -110,7 +110,7 @@ function birdCatchY(){
 }
 function birdCatchG(){
   for(let i=0;i<birdsGreen.length;i++){
-    if(dist(birdsGreen[i].x,birdsGreen[i].y, gX, gY) <= bsize+10){
+    if(dist(birdsGreen[i].x,birdsGreen[i].y, gX, gY) <= bsize+15){
       birdsGreen.splice(i,1);
 
       Green[2] += 1;
@@ -121,7 +121,7 @@ function birdCatchG(){
 //yellow user
 function birdCatchB(){
   for(let i=0;i<birdsBlue.length;i++){
-    if(dist(birdsBlue[i].x,birdsBlue[i].y, yX, yY) <= bsize+10){
+    if(dist(birdsBlue[i].x,birdsBlue[i].y, yX, yY) <= bsize+15){
       birdsBlue.splice(i,1);
 
       Yellow[1] += 1;
@@ -131,7 +131,7 @@ function birdCatchB(){
 
 function birdCatchR(){
   for(let i=0;i<birdsRed.length;i++){
-    if(dist(birdsRed[i].x,birdsRed[i].y, yX, yY) <= bsize+10){
+    if(dist(birdsRed[i].x,birdsRed[i].y, yX, yY) <= bsize+15){
       birdsRed.splice(i,1);
 
       Yellow[0] += 1;
@@ -140,7 +140,7 @@ function birdCatchR(){
 }
 function birdCatchY(){
   for(let i=0;i<birdsYellow.length;i++){
-    if(dist(birdsYellow[i].x,birdsYellow[i].y, yX, yY) <= bsize+10){
+    if(dist(birdsYellow[i].x,birdsYellow[i].y, yX, yY) <= bsize+15){
       birdsYellow.splice(i,1);
 
       Yellow[3] += 1;
@@ -150,7 +150,7 @@ function birdCatchY(){
 }
 function birdCatchG(){
   for(let i=0;i<birdsGreen.length;i++){
-    if(dist(birdsGreen[i].x,birdsGreen[i].y, yX, yY) <= bsize+10){
+    if(dist(birdsGreen[i].x,birdsGreen[i].y, yX, yY) <= bsize+15){
       birdsGreen.splice(i,1);
 
       Yellow[2] += 1;
