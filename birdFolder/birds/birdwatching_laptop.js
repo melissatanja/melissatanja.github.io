8e3596705355f0336457ad0e8fbd0a2b6890c25a
melissatanja <43143959+melissatanja@users.noreply.github.com>
2019-01-24 01:08:20 -0500

    n

--- birdFolder/birds/birdwatching_laptop.js
+++ birdFolder/birds/birdwatching_laptop.js
@@ -156,7 +156,7 @@ function setup()
   dataServer.addListener({ message: readIncoming });
   dataServer.subscribe({channels: [channelName, tradeChannel]});
 
-  console.log("update3");
+  console.log("update4");
 
   //setup birds
   for (let i = 0; i < 4; i++) {
