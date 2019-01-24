9d3c1696ae2ea63acc9d92e1af314fd808c99514
melissatanja <43143959+melissatanja@users.noreply.github.com>
2019-01-24 01:14:31 -0500

    n

--- birdFolder/birds/birdwatching_laptop.js
+++ birdFolder/birds/birdwatching_laptop.js
@@ -205,13 +205,13 @@ function draw()
 
   // red binoculars
   if(r_xpos != undefined && r_ypos != undefined){
-    r_X = map(r_xpos, -10, 10,  45, cs/2 - 45);
-    r_Y = map(r_ypos, -10, 10, cs/2 - 45, 45);
+    r_X = map(r_xpos, -10, 10,  45, width/2 - 45);
+    r_Y = map(r_ypos, -10, 10, height/2 - 45, 45);
     r_prevX = rX;
     r_prevY = rY;
   }else{
-    rX = map(r_prevX, -10, 10,  45, cs/2 - 45);
-    rY = map(r_prevY, -10, 10, cs/2 - 45, 45);
+    rX = map(r_prevX, -10, 10,  45, width/2 - 45);
+    rY = map(r_prevY, -10, 10, height/2 - 45, 45);
   }
 
   // if(r_xpos != undefined && r_ypos != undefined){
